a133c2ebfb5340abe17b84b9f63c138e5fc78fe9
Declan Halpin <declanhalpin@Declans-MacBook-Pro.local>
2020-03-09 11:07:58 +0000

    first commit

---
+++ src/main/java/com/openshift/osdemo/OsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.openshift.osdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsdemoApplication.class, args);
+	}
+
+}
